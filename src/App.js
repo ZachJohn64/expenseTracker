a53f0b6d7dcd12e8bb1804f52acbb328ec7b438e
ZachJohn64 <zachdjohnson64@gmail.com>
2020-07-16 15:46:59 -0400

    Displayed Expenses and implemented up-to-date balance

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { History } from './components/History';
 import { NewTransaction } from './components/NewTransaction';
+import { GlobalProvider } from './context/GlobalState'
+
 import './App.css';
 
 function App() {
   return (
-    <>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -16,7 +18,7 @@ function App() {
         <History />
         <NewTransaction />
       </div>
-    </>
+    </GlobalProvider>
   );
 }
 
