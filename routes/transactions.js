48ac526c550483296995c0d50cbb02c799f3088e
ZachJohn64 <zachdjohnson64@gmail.com>
2020-07-18 12:05:28 -0400

    reverted back to old router

--- routes/transactions.js
+++ routes/transactions.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { getTransactions, addTransactions, removeTransactions } = require('../controllers/transaction_controller')
 
 router
-    .route('https://enigmatic-inlet-18390.herokuapp.com/')
+    .route('/')
     .get(getTransactions)
     .post(addTransactions)
 
 router
-    .route('https://enigmatic-inlet-18390.herokuapp.com/:id')
+    .route('/:id')
     .delete(removeTransactions)
 
 module.exports = router;
