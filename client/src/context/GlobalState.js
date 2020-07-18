c229f83da584027ff5da9a62be4c6781924d405e
ZachJohn64 <zachdjohnson64@gmail.com>
2020-07-18 10:17:26 -0400

    Hooked up getTransactions and deleteTransactions to global state. Added morgan.

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,14 +1,12 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
+import axios from 'axios'
 
 //Initial State
 const initialState = {
-    transactions: [
-        // {id: 1, text: 'Flower', amount: -20},
-        // {id: 2, text: 'Salary', amount: 300},
-        // {id: 3, text: 'Book', amount: -10},
-        // {id: 4, text: 'Camera', amount: -150}
-    ]
+    transactions: [],
+    error: null,
+    loading: true
 }
 
 // Create context
@@ -18,12 +16,38 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    
     // Actions
-    function deleteTransaction(id) {
-        dispatch({
-            type: 'DELETE_TRANSACTION',
-            payload: id
-        });
+    async function getTransactions() {
+        try {
+            const res = await axios.get('api/v1/transactions');
+
+            dispatch({
+                type: 'GET_TRANSACTIONS',
+                payload: res.data.data
+            })
+        } catch (err) {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.date.error
+            })
+        }
+    }
+
+    async function deleteTransaction(id) {
+        try {
+            await axios.delete(`/api/v1/transactions/${id}`);
+
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                payload: id
+            });
+        } catch (err) {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.date.error
+            });
+        }        
     }
     function addTransaction(transaction) {
         dispatch({
@@ -35,6 +59,9 @@ export const GlobalProvider = ({ children }) => {
     return (
         <GlobalContext.Provider value={{
             transactions: state.transactions,
+            error: state.error,
+            loading: state.loading,
+            getTransactions, 
             deleteTransaction,
             addTransaction
         }}>
